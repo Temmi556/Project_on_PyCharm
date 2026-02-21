0315e6b4cb74d460d8a1deeb663189f334cb9388
Temmik556 <temmi556@mail.ru>
2026-02-21 20:54:29 +0300

    Obnova

--- method_string.py
+++ method_string.py
@@ -1,33 +1,17 @@
-# name = "Tema or or hoh jojo"
-# print(name.upper())
-# print(name.lower())
-# print(name.title())
-# print(name.split(""))
+from random import shuffle
 
-# name = "Tema"
-# age = 18
-# name_and_age_1 = "My name is {0} and I am {1} years old".format(name, age)
-# name_and_age_2 = f"My name is{name} and I am {age} years old"
-# print(name_and_age_1)
-# print(name_and_age_2)
+a = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+shuffle(a)
+print(a)
 
-# number = 10000/18
-# print(f"{number:.2f}")
-# print(f"{number:.2f}")
-# list_1 = [0, 1, 2, 3, 4, 5, 6, 7]
-# list_1.append(8)
-# print(list_1)
-# list_1.insert(1,0.5)
-# print(list_1)
-# list_1.pop(2)
-# list_1.remove(2)
-# print(list_1)
-# list_2 = [19, 234, 9, 0, 3748, 231, 23456]
-# list_2.sort()
-# print(list_2)
-# list_2.reverse()
-# print(list_2)
 
-string = "Hello World"
-for i in enumerate(string):
-    print(i)
+def print_hello():
+    """
+    This function prints "Hello!" to the console./
+    Not return anything.
+    :return: None
+    """
+    print("Hello!")
+
+
+print_hello()
