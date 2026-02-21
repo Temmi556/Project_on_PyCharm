01b42071b55f1bc2f15c3c8393d39b2f54ba661a
Temmik556 <temmi556@mail.ru>
2026-02-21 12:10:28 +0300

    As

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-# a = 5
-# b = 6
-# print(a + b)
-first_name = "John"
-last_name = "Doe"
-full_name = first_name + last_name
-print(full_name)
+def main():
+    first_name = "John"
+    last_name = " Doe"
+    full_name = first_name + 5 + last_name
+    print(full_name)
+
+
+main()
