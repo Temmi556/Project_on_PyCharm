961ef0d93837e1c06b7675e698d7aa3e329813cb
Temmik556 <temmi556@mail.ru>
2026-02-21 13:15:58 +0300

    Obnova

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def main():
-    first_name = "John"
-    last_name = " Doe"
-    full_name = first_name + 4 + last_name
-    print(full_name)
-
-
-main()
+first_name = "John"
+last_name = " Doe"
+full_name = first_name + last_name
+print(full_name[0])
+str_1 = "Hello World"
+print(str_1[0:5])
