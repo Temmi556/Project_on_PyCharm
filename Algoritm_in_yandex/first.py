7c85deac34a3aaa9d0d1428c0cd1551163238172
Temmik556 <temmi556@mail.ru>
2026-02-25 16:23:51 +0300

    Переименование_функции

--- Algoritm_in_yandex/first.py
+++ Algoritm_in_yandex/first.py
@@ -1,23 +1,24 @@
 import sys
 
 
-def main():
-    sp = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', '10#':'j',
+def function_for_f():
+      sp = {1:'a', 2:'b', 3:'c', 4:'d', 5:'e', 6:'f', 7:'g', 8:'h', 9:'i', '10#':'j',
       '11#':'k','12#':'l', '13#':'m', '14#':'n', '15#':'o', '16#':'p', '17#':'q',
       '18#':'r', '19#':'s', '20#':'t', '21#':'u', '22#':'v', '23#':'w', '24#':'x',
       '25#':'y', '26#':'z'}
-    s = input()
-    b = ''
-    i = 0
-    while i < len(s):
-        if i + 2 < len(s) and s[i+2] == '#':
-            b += sp[s[i]+s[i+1]+'#']
-            i += 3
-        else:
-            b += sp[int(s[i])]
-            i += 1
-    return b
-
+      s = input()
+      b = ''
+      i = 0
+      while i < len(s):
+            if i + 2 < len(s) and s[i+2] == '#':
+                b += sp[s[i]+s[i+1]+'#']
+                i += 3
+            else:
+                b += sp[int(s[i])]
+                i += 1
+      return b
+    
 
 if __name__ == '__main__':
-    main()
+    a = function_for_f()
+    print(a)
