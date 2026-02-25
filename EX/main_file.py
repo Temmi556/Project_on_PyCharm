82e8d173b0b129e93a13f97e60dc9864400aca59
Temmik556 <temmi556@mail.ru>
2026-02-25 19:17:15 +0300

    Добавление задач по __name__ = "__main__"

---
+++ EX/main_file.py
@@ -0,0 +1,7 @@
+from utils import get_favorite_color, get_favorite_number
+
+
+color = get_favorite_color()
+number = get_favorite_number()
+
+print(f"My favoritesssss color is {color} and my favorite number is {number}.")
---
+++ EX/utils.py
@@ -0,0 +1,10 @@
+def get_favorite_color():
+    return "super-duper color"
+
+
+
+def get_favorite_number():
+    return 13
+
+if __name__ == "__main__":
+    print(f"My favorite color is {get_favorite_color()} and my favorite number is {get_favorite_number()}.")
